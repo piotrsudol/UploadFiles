bca2b3890ed420b7bb3c14aeb6fbf3928f69c06e
piotr.olender <piotr.olender@shijigroup.com>
2022-09-30 21:21:24 +0200

    Uploading files

---
+++ FileManager/Controllers/FileController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileController : ControllerBase
+    {
+        private readonly IWebHostEnvironment hosting;
+
+        public FileController(IWebHostEnvironment hosting)
+        {
+            this.hosting = hosting;
+        }
+
+        [HttpPost("singleFile")]
+        public async Task<IActionResult> SingleFile(IFormFile file)
+        {
+            var dir = hosting.ContentRootPath;
+
+            await CreateFile(file, Path.Combine(dir, file.FileName));
+
+            return Ok();
+        }
+
+        private async Task CreateFile(IFormFile file, string path) {
+
+            using (var fileStream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+        }
+
+        [HttpPost("MultipleFiles")]
+        public IActionResult MultipleFiles(IEnumerable<IFormFile> file)
+        {
+            var dir = hosting.ContentRootPath;
+
+            Task.WaitAll(
+                file.Select(x =>
+                {
+                    return CreateFile(x, Path.Combine(dir, x.FileName));
+                }).ToArray());
+
+            return Ok();
+        }
+    }
+}
